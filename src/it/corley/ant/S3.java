7cafed6a817b87a65e81b75ac4045dbe894e9b39
Walter Dal Mut <walter.dalmut@gmail.com>
2012-04-16 11:06:17 +0200

    First time upload

--- src/it/corley/ant/S3.java
+++ src/it/corley/ant/S3.java
@@ -8,13 +8,17 @@ import org.apache.tools.ant.Task;
 
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.PutObjectResult;
 
 public class S3 extends Task {
 
 	private String key;
 	private String secret;
 	private String bucket;
+	private String dest;
 	private String bundleUpload;
 	boolean fail = false;
 	
@@ -38,6 +42,11 @@ public class S3 extends Task {
 		this.bundleUpload = bundleUpload;
 	}
 	
+	public void setDest(String dest)
+	{
+		this.dest = dest;
+	}
+	
     public void setFail(boolean b) {
         fail = b;
     }
@@ -46,7 +55,14 @@ public class S3 extends Task {
 	{
 		if (fail) throw new BuildException("Fail requested.");
 		
-		//AWSCredentials credential = new BasicAWSCredentials(key, secret);
-		log(bundleUpload);
+		AWSCredentials credential = new BasicAWSCredentials(key, secret);
+		AmazonS3 s3 = new AmazonS3Client();
+		
+		File file = new File(bundleUpload);
+		PutObjectRequest por = new PutObjectRequest(bucket, dest, file);
+
+		PutObjectResult result = s3.putObject(por);
+
+		log("File uploaded: " + bundleUpload);
 	}
 }
