afb529129ab5f943f71a6c27438c3e4d1848e97d
wdalmut <walter.dalmut@gmail.com>
2013-02-17 10:35:42 +0100

    Allow all Cache-Control properties instead max-age only and bug fix on s3 endpoint

--- src/test/it/corley/ant/S3PutTaskTest.java
+++ src/test/it/corley/ant/S3PutTaskTest.java
@@ -1,21 +1,6 @@
 package it.corley.ant;
 
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.*;
-import junit.framework.Assert;
 import junit.framework.TestCase;
-
-import org.apache.tools.ant.BuildException;
-import org.apache.tools.ant.Project;
-import org.apache.tools.ant.types.FileSet;
-import org.apache.tools.ant.types.selectors.FileSelector;
-import org.apache.tools.ant.types.selectors.FilenameSelector;
-
-import java.io.*;
-import java.util.Properties;
 
 /**
  * Unit test class for the S3PutTask class
